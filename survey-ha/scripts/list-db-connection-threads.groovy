91039e0590205a80b483af9c02c14129045ac5e3
chris grzegorczyk <grze@eucalyptus.com>
2013-08-02 20:01:15 -0700

    Add -T which checks each db connection state and owning threads stack trace

---
+++ survey-ha/scripts/list-db-connection-threads.groovy
@@ -0,0 +1,14 @@
+import org.logicalcobwebs.proxool.ConnectionPoolManager
+import org.logicalcobwebs.proxool.ProxyConnection
+import com.eucalyptus.entities.PersistenceContexts;
+
+PersistenceContexts.list( ).collect{ ctx ->
+  ConnectionPoolManager.getInstance().getConnectionPool(ctx).getProxyConnections( ).collect{ ProxyConnection p ->
+    stack = Thread.getAllStackTraces( ).findAll{ thread -> thread?.getKey()?.getName( ) == p.getRequester( ) }.collect{ Arrays.asList(it.getValue()) }
+    """
+${p.id} active=${p.isActive( )} available=${p.isAvailable( )} closed=${p.isClosed( )} expiring=${p.isMarkedForExpiry( )} null=${p.isNull( )} offline=${p.isOffline( )} really-closed=${p.isReallyClosed( )} age=${p.age} birthdate=${p.getBirthDate( )} reason=${p.getReasonForMark( )}
+${p.id} last-sql=${p.getLastSqlCall( )}
+${p.id} owning-thread=${p.getRequester( )}
+${p.id} """ + stack.collect{it.join("\n\t")}
+  }
+}
