2ae2cd9f67eeec829f77ae432fde0646c38223de
chris grzegorczyk <grze@eucalyptus.com>
2013-08-06 15:43:52 -0700

    Restrict inspected connections to eucalyptus_cloud and only in-use connections

--- survey-ha/scripts/list-db-connection-threads.groovy
+++ survey-ha/scripts/list-db-connection-threads.groovy
@@ -17,7 +17,7 @@ output = """
 ${p.id} active=${p.isActive( )} available=${p.isAvailable( )} closed=${p.isClosed( )} expiring=${p.isMarkedForExpiry( )} null=${p.isNull( )} offline=${p.isOffline( )} really-closed=${p.isReallyClosed( )} age=${p.age/1000.0}secs birthdate=${p.getBirthDate( )} reason=${p.getReasonForMark( )}
 ${p.id} last-sql=${p.getLastSqlCall( )}
 ${p.id} owning-thread=${p.getRequester( )}
-${p.id} """ + stack.collect{it.join("\n\t")}
+${p.id} ${stack.collect{it.join("\n\t")}}"""
 	LOG.info(output)
 	output
   }
