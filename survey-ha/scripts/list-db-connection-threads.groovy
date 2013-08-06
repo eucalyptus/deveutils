2a3d7b0245ced128a317df95081319157e0a3ded
chris grzegorczyk <grze@eucalyptus.com>
2013-08-06 15:05:25 -0700

    Restrict inspected connections to eucalyptus_cloud and only in-use connections

--- survey-ha/scripts/list-db-connection-threads.groovy
+++ survey-ha/scripts/list-db-connection-threads.groovy
@@ -2,12 +2,17 @@ import org.logicalcobwebs.proxool.ConnectionPoolManager
 import org.logicalcobwebs.proxool.ProxyConnection
 import com.eucalyptus.entities.PersistenceContexts;
 
+def finder = { ProxyConnection p ->
+	p.isActive() || p.isClosed()
+}
 //PersistenceContexts.list( ).collect{ ctx ->
-["eucalyptus_cloud","eucalyptus_auth"].collect{ ctx ->
-  ConnectionPoolManager.getInstance().getConnectionPool(ctx).getProxyConnections( ).findAll{ it.isActive() || it.isClosed() }.collect{ ProxyConnection p ->
+["eucalyptus_cloud"].collect{ ctx ->
+  ConnectionPoolManager.getInstance().getConnectionPool(ctx).getProxyConnections( ).findAll{ 
+  	finder(it)
+  }.collect{ ProxyConnection p ->
     stack = Thread.getAllStackTraces( ).findAll{ thread -> thread?.getKey()?.getName( ) == p.getRequester( ) }.collect{ Arrays.asList(it.getValue()) }
     """
-${p.id} active=${p.isActive( )} available=${p.isAvailable( )} closed=${p.isClosed( )} expiring=${p.isMarkedForExpiry( )} null=${p.isNull( )} offline=${p.isOffline( )} really-closed=${p.isReallyClosed( )} age=${p.age} birthdate=${p.getBirthDate( )} reason=${p.getReasonForMark( )}
+${p.id} active=${p.isActive( )} available=${p.isAvailable( )} closed=${p.isClosed( )} expiring=${p.isMarkedForExpiry( )} null=${p.isNull( )} offline=${p.isOffline( )} really-closed=${p.isReallyClosed( )} age=${p.age/1000.0}secs birthdate=${p.getBirthDate( )} reason=${p.getReasonForMark( )}
 ${p.id} last-sql=${p.getLastSqlCall( )}
 ${p.id} owning-thread=${p.getRequester( )}
 ${p.id} """ + stack.collect{it.join("\n\t")}
