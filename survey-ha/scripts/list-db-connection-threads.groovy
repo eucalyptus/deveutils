1f6647392603e9409dac624d8a70352489b89e07
chris grzegorczyk <grze@eucalyptus.com>
2013-08-06 15:42:03 -0700

    Restrict inspected connections to eucalyptus_cloud and only in-use connections

--- survey-ha/scripts/list-db-connection-threads.groovy
+++ survey-ha/scripts/list-db-connection-threads.groovy
@@ -1,7 +1,9 @@
 import org.logicalcobwebs.proxool.ConnectionPoolManager
 import org.logicalcobwebs.proxool.ProxyConnection
 import com.eucalyptus.entities.PersistenceContexts;
+import org.apache.log4j.Logger
 
+Logger LOG = Logger.getLogger("list-db-connection-threads")
 def finder = { ProxyConnection p ->
 	p.isActive() || p.isClosed()
 }
@@ -11,10 +13,12 @@ def finder = { ProxyConnection p ->
   	finder(it)
   }.collect{ ProxyConnection p ->
     stack = Thread.getAllStackTraces( ).findAll{ thread -> thread?.getKey()?.getName( ) == p.getRequester( ) }.collect{ Arrays.asList(it.getValue()) }
-    """
+output = """
 ${p.id} active=${p.isActive( )} available=${p.isAvailable( )} closed=${p.isClosed( )} expiring=${p.isMarkedForExpiry( )} null=${p.isNull( )} offline=${p.isOffline( )} really-closed=${p.isReallyClosed( )} age=${p.age/1000.0}secs birthdate=${p.getBirthDate( )} reason=${p.getReasonForMark( )}
 ${p.id} last-sql=${p.getLastSqlCall( )}
 ${p.id} owning-thread=${p.getRequester( )}
 ${p.id} """ + stack.collect{it.join("\n\t")}
+	LOG.info(output)
+	output
   }
 }
